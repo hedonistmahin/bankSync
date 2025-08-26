3e5dd3d2c31234fc514a7df1d47dec165c89dfc5
hedonistmahin <mdmodabbirmahin@gmail.com>
2025-08-27 03:40:36 +0600

    Prepare for Vercel deployment

--- server.js
+++ server.js
@@ -182,4 +182,5 @@ app.get('/api/stats', authenticate, async (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`Server running at http://localhost:${3000}`));
+// Export the app as a serverless function for Vercel
+module.exports = app;
